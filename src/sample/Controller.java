4e3c49fdfbbb3aa6a47f304b988c194687a97b23
MathiasMadsen <m1999@live.dk>
2020-11-25 14:51:26 +0100

    fejl

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+
 public class Controller {
+
+    //kobler XML med java
+    @FXML
+    private TextArea tekst1;
+
+    public void knapKlikket(){
+        System.out.println("Knappen blev klikket på");
+        tekst1.append("Det virker");
+    }
+
 }
